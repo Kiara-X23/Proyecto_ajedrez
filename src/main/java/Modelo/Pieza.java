699b5ce362ab1ab20beb73159ff6db3268c64be9
ExplorerDyl <149419817+ExplorerDyl@users.noreply.github.com>
2023-11-19 02:08:56 -0600

    Update Pieza.java

--- src/main/java/Modelo/Pieza.java
+++ src/main/java/Modelo/Pieza.java
@@ -1,69 +1,47 @@
-
-
 package Modelo;
 
 public abstract class Pieza {
-    private String tipo;
-    private String color;
-    private int posX;
-    private int posY;
-    
-    
-     
-    public Pieza(String tipo, String color, int posX, int posY) {
+    private TipoPieza tipo;
+    private Color color;
+    private int x;
+    private int y;
+
+    public Pieza(TipoPieza tipo, Color color, int x, int y) {
         this.tipo = tipo;
         this.color = color;
-        this.posX = posX;
-        this.posY = posY;
+        this.x = x;
+        this.y = y;
     }
-    
- 
-    
-    public String getTipo() {
+
+    public abstract void mover(int x, int y);
+
+    public abstract boolean validarMovimiento(int x, int y);
+
+    public abstract boolean isCaptura(int posX, int posY);
+
+    // Getters y setters para obtener y establecer la posición de la pieza
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Otros getters para obtener información sobre la pieza
+    public TipoPieza getTipo() {
         return tipo;
     }
-    
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-    
-    public String getColor() {
+
+    public Color getColor() {
         return color;
     }
-    
-    public void setColor(String color) {
-        this.color = color;
-    }
-    
-    public int getPosX() {
-        return posX;
-    }
-    
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-    
-    public int getPosY() {
-        return posY;
-    }
-    
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
-  
-    public boolean validarMovimiento(int nuevaPosX, int nuevaPosY) {
-        
-        return true; 
-    }
-    
-    
-
 }
-
-
-    
-    
-    
-
-    
-   
