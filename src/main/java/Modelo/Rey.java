f9e9549ca4d222d96b46a25042e3b7b996624d39
ExplorerDyl <149419817+ExplorerDyl@users.noreply.github.com>
2023-11-19 03:29:11 -0600

    Update Rey.java

--- src/main/java/Modelo/Rey.java
+++ src/main/java/Modelo/Rey.java
@@ -1,28 +1,40 @@
-
 package Modelo;
 
-
 public class Rey extends Pieza {
-    
-private boolean estadoJaque;
 
-    public Rey(String tipo, String color, int posX, int posY) {
-        super(tipo, color, posX, posY);
+    public Rey(Color color, int x, int y) {
+        super(TipoPieza.REY, color, x, y);
     }
-    
-    
-public void Mover() {
-        
+
+    @Override
+    public void mover(int x, int y) {
+        // Implementación del movimiento del rey
+        if (validarMovimiento(x, y)) {
+            setX(x);
+            setY(y);
+        } else {
+            System.out.println("Movimiento inválido para el rey.");
+        }
     }
-    
-public void validarMovimiento (){
-      
-        
-    }  
-  
-public boolean isCaptura() {
-        
-        return false;
-        
-    } 
+
+    @Override
+    public boolean validarMovimiento(int x, int y) {
+        // Implementación de la validación del movimiento del rey
+        int diffX = Math.abs(x - getX());
+        int diffY = Math.abs(y - getY());
+
+        return (diffX <= 1 && diffY <= 1);
+    }
+
+    @Override
+    public boolean isCaptura(int posX, int posY) {
+        // Implementación para verificar si el rey puede capturar en la posición (posX, posY)
+        return validarMovimiento(posX, posY) && (getColor() != obtenerColorEnPosicion(posX, posY));
+    }
+
+    private Color obtenerColorEnPosicion(int x, int y) {
+        // Método para obtener el color de la pieza en la posición (x, y)
+        // Implementación similar a la utilizada en las clases anteriores
+        // ...
+    }
 }
