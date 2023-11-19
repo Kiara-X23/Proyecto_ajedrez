0c20dec922f001484772e8d1157bed8e43de3f05
ExplorerDyl <149419817+ExplorerDyl@users.noreply.github.com>
2023-11-19 03:03:16 -0600

    Update Caballo.java

--- src/main/java/Modelo/Caballo.java
+++ src/main/java/Modelo/Caballo.java
@@ -4,23 +4,47 @@ package Modelo;
 
 public class Caballo extends Pieza {
 
-    public Caballo(String tipo, String color, int posX, int posY) {
-        super(tipo, color, posX, posY);
+    public Caballo(Color color, int x, int y) {
+        super(Ttipo.CABALLO, color, x, y);
     }
-    
-  public void Mover() {
-        
+
+    @Override
+    public void mover(int x, int y) {
+        // Implementación del movimiento del caballo
+        if (validarMovimiento(x, y)) {
+            setX(x);
+            setY(y);
+        } else {
+            System.out.println("Movimiento inválido para el caballo.");
+        }
     }
-    
-  public void validarMovimiento (){
-      
-        
-    }  
-  
-  public boolean isCaptura() {
-        
-        return false;
-        
+
+    @Override
+    public boolean validarMovimiento(int x, int y) {
+        // Implementación de la validación del movimiento del caballo
+        int diffX = Math.abs(x - getX());
+        int diffY = Math.abs(y - getY());
+
+        return (diffX == 1 && diffY == 2) || (diffX == 2 && diffY == 1);
     }
-    
+
+    @Override
+    public boolean isCaptura(int posX, int posY) {
+        // Implementación para verificar si el caballo puede capturar en la posición (posX, posY)
+        return validarMovimiento(posX, posY) && (getColor() != obtenerColorEnPosicion(posX, posY));
+    }
+
+    private Color obtenerColorEnPosicion(int x, int y) {
+        // Método para obtener el color de la pieza en la posición (x, y) si hay alguna
+        // Por simplicidad, asumiré que existe una clase Tablero con un método obtenerPieza()
+        // que devuelve la pieza en una posición específica
+        // Este método puede variar según cómo se implemente la lógica en tu proyecto
+
+        Pieza pieza = Tablero.obtenerPieza(x, y); // Reemplaza Tablero.obtenerPieza() con el método correspondiente en tu proyecto
+
+        if (pieza != null) {
+            return pieza.getColor();
+        }
+        return null; // Si no hay ninguna pieza en esa posición
+    }
 }
