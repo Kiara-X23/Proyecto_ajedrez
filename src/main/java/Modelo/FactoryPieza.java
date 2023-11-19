7a7cb057b13d44193582422af3140a41f333bbc2
ExplorerDyl <149419817+ExplorerDyl@users.noreply.github.com>
2023-11-19 12:52:10 -0600

    Update FactoryPieza.java

--- src/main/java/Modelo/FactoryPieza.java
+++ src/main/java/Modelo/FactoryPieza.java
@@ -4,11 +4,23 @@ package Modelo;
 
 public class FactoryPieza {
     
-     public Pieza factoyMethod(Ttipo tipo, TColor color) {
-         
-        
-         return null;
-        
+     public Pieza factoryMethod(TipoPieza tipo, Color color, int x, int y) {
+        switch (tipo) {
+            case PEON:
+                return new Peon(color, x, y);
+            case TORRE:
+                return new Torre(color, x, y);
+            case CABALLO:
+                return new Caballo(color, x, y);
+            case ALFIL:
+                return new Alfil(color, x, y);
+            case REINA:
+                return new Reina(color, x, y);
+            case REY:
+                return new Rey(color, x, y);
+            default:
+                throw new IllegalArgumentException("Tipo de pieza desconocido: " + tipo);
+        }
     }
 
 
